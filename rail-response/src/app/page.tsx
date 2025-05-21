4f04b43f59519854e6f6b155daf90c46f9697088
alanerochaa <alanerochaa@gmail.com>
2025-05-21 01:57:13 -0300

    atualizacoes

--- rail-response/src/app/page.tsx
+++ rail-response/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Logo } from "../components/Logo"; // ajuste o caminho conforme sua estrutura
+import { Logo } from "../components/Logo"; 
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -12,12 +12,10 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#1b4263] text-center">
-      {/* Logo com alt para acessibilidade */}
+      
 <div role="img" aria-label="Logo da CCR RailResponse" className="rounded-full">
-  <Logo />
 </div>
 
-      {/* Nome do sistema com aria-label */}
       <h1
         className="text-orange-500 text-4xl font-bold mt-4"
         aria-label="Sistema de Gestão de Alertas Ferroviários"
@@ -25,7 +23,6 @@ export default function Home() {
         RailResponse
       </h1>
 
-      {/* Botão de entrada */}
       <button
         onClick={handleClick}
         className="mt-6 bg-orange-500 text-black font-medium text-lg px-6 py-2 rounded hover:bg-orange-600 transition"
